9d021c6122121a1d4d355d3cc8c8f8069a4a8aeb
BoMaasen <bomaassen@live.nl>
2025-01-08 10:31:21 +0100

    get request en state aangemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+import axios from "axios";
+import {useState} from "react";
 
 function App() {
+    const [pokemon, setPokemon] = useState([]);
+    const [error, toggleError] = useState(false);
+
+    async function fetchPokemon(){
+        toggleError(false);
+        try {
+            const result = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+            console.log(result.data);
+            setPokemon(result.data);
+        }catch (e){
+            console.log("er ging iets fout, probeer het opniew! " + e);
+            toggleError(true);
+        }
+    }
+
+    fetchPokemon()
 
   return (
     <>
       <h1>Gotta catch em all!</h1>
+
+
     </>
   )
 }
